aad7a8f7063778d76310be8336ac8ece12efb38f
Yoav Kleinberger <haarcuba@gmail.com>
2024-09-12 20:24:05 +0300

    basic worky

--- foxyy/main.py
+++ foxyy/main.py
@@ -1,7 +1,19 @@
 import click
+import foxyy.imports
 
+def red(text):
+    return click.secho(text, fg='red', bold=True)
+
+def normal(text):
+    return click.secho(text)
 
 @click.command()
 @click.argument('files', nargs=-1, type=click.Path())
 def main(files):
-    click.echo(files)
+    findings = foxyy.imports.enforce(files)
+    for file, analysis in findings.items():
+        if analysis['ok']:
+            continue
+        red(f'{file} has non-foxyy imports:')
+        for error in analysis['errors']:
+            normal(f'  {error}')
