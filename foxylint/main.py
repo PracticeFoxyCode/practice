a654f9c607f280d4a893ea8d2c067267f69f626e
Yoav Kleinberger <haarcuba@gmail.com>
2024-09-13 00:40:54 +0300

    support for accept

--- foxylint/main.py
+++ foxylint/main.py
@@ -1,4 +1,5 @@
 import click
+import os
 import glob
 import foxylint.imports
 
@@ -18,22 +19,30 @@ def normal(text):
 @click.command()
 @click.argument('files', nargs=-1, type=click.Path())
 @click.option('--exclude', '-e', multiple=True)
-def main(files, exclude):
+@click.option('--accept', multiple=True)
+def main(files, exclude, accept):
     excluded = []
     for pattern in exclude:
         excluded.extend(glob.glob(pattern))
 
-    files = [file for file in files if file not in excluded]
-    findings = foxylint.imports.analyze(files)
+    excluded = [str(os.path.abspath(file)) for file in excluded]
+
+    candidates = [file for file in files if file not in excluded]
+    files = []
+    for file in candidates:
+        if os.path.abspath(file) not in excluded:
+            files.append(file)
+
+    findings = foxylint.imports.analyze(files, acceptable_patterns=accept)
     bad_files = 0
     for file, analysis in findings.items():
         if analysis['ok']:
             continue
         bad_files += 1
-        red(f'{file} has non-foxy imports:')
+        red(f'{file}:')
         for error in analysis['errors']:
             line, content = error['line_number'], error['line_content']
-            normal(f'  {line}: {content}')
+            normal(f'  {file}:{line}: {content}')
 
     if bad_files > 0:
         red(f'found {bad_files} out of {len(files)} files with non-foxy imports.')
