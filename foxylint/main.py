51810149df47f8c3e74cdbf342648d19ffc2c97c
Yoav Kleinberger, PhD <haarcuba@gmail.com>
2024-09-13 01:22:19 +0300

    fix some issues (#15)
    * fix printing
    * update readme
    * bump version to 0.7.0

--- foxylint/main.py
+++ foxylint/main.py
@@ -8,12 +8,8 @@ def red(text):
     return click.secho(text, fg='red', bold=True)
 
 
-def green(text):
-    return click.secho(text, fg='green', bold=True)
-
-
-def normal(text):
-    return click.secho(text)
+def bold(text):
+    return click.secho(text, fg='white', bold=True)
 
 
 @click.command()
@@ -42,11 +38,9 @@ def main(files, exclude, accept):
         red(f'{file}:')
         for error in analysis['errors']:
             line, content = error['line_number'], error['line_content']
-            normal(f'  {file}:{line}: {content}')
+            bold(f'  {file}:{line}: {content}')
 
     if bad_files > 0:
-        red(f'found {bad_files} out of {len(files)} files with non-foxy imports.')
         quit(1)
     else:
-        green(f'all {len(files)} files have foxy imports.')
         quit(0)
